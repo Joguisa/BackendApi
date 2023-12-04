e6815ec3ab1ddb3e4025c4efcf8d301a0599ee19
jonatan <jntnglln@gmail.com>
2023-12-03 23:24:33 -0500

    Agregar archivos de proyecto.

---
+++ BackendApi/Models/DBEmpleadoContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BackendApi.Models
+{
+    public partial class DBEmpleadoContext : DbContext
+    {
+        public DBEmpleadoContext()
+        {
+        }
+
+        public DBEmpleadoContext(DbContextOptions<DBEmpleadoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Departamento> Departamentos { get; set; } = null!;
+        public virtual DbSet<Empleado> Empleados { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Departamento>(entity =>
+            {
+                entity.HasKey(e => e.IdDepartamento)
+                    .HasName("PK__Departam__787A433DA5A04EFE");
+
+                entity.ToTable("Departamento");
+
+                entity.Property(e => e.FechaCreacion)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Empleado>(entity =>
+            {
+                entity.HasKey(e => e.IdEmpleado)
+                    .HasName("PK__Empleado__CE6D8B9EC00ED102");
+
+                entity.ToTable("Empleado");
+
+                entity.Property(e => e.FechaContrato)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.NombreCompleto)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdDepartamentoNavigation)
+                    .WithMany(p => p.Empleados)
+                    .HasForeignKey(d => d.IdDepartamento)
+                    .HasConstraintName("FK__Empleado__IdDepa__4CA06362");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ BackendApi/Models/Departamento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BackendApi.Models
+{
+    public partial class Departamento
+    {
+        public Departamento()
+        {
+            Empleados = new HashSet<Empleado>();
+        }
+
+        public int IdDepartamento { get; set; }
+        public string? Nombre { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+
+        public virtual ICollection<Empleado> Empleados { get; set; }
+    }
+}
